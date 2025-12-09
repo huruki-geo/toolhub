3a68e5012448a03799fc6017fa7d64e8fb43bb38
huruki-geo <lot.ofkzmnpc@gmail.com>
2025-12-09 10:40:58 +0900

    Update sitemap.xml.ts

--- functions/sitemap.xml.ts
+++ functions/sitemap.xml.ts
@@ -1,6 +1,18 @@
 export const onRequestGet = async (context) => {
-  // 静的ファイルの sitemap.xml を返す
-    const url = new URL("sitemap.xml", context.request.url);
+  const url = new URL("sitemap.xml", context.request.url);
 
-  return context.env.ASSETS.fetch(url.toString());
+  // ASSETS（dist/）にある sitemap.xml を取得
+  return await context.env.ASSETS.fetch(url.toString());
+};
+
+export const onRequestHead = async (context) => {
+  const url = new URL("sitemap.xml", context.request.url);
+
+  const res = await context.env.ASSETS.fetch(url.toString());
+
+  // HEAD：ボディなし、ヘッダーだけ返す
+  return new Response(null, {
+    status: res.status,
+    headers: res.headers,
+  });
 };

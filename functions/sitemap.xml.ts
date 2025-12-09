f3bd5ac822e8ab3c57b760e8ab8af6e4c56ffbfc
huruki-geo <lot.ofkzmnpc@gmail.com>
2025-12-09 10:17:41 +0900

    Update sitemap.xml.ts

--- functions/sitemap.xml.ts
+++ functions/sitemap.xml.ts
@@ -1,4 +1,4 @@
 export const onRequestGet = async (context) => {
   // 静的ファイルの sitemap.xml を返す
-  return await context.env.ASSETS.fetch("/sitemap.xml");
+  return await context.env.ASSETS.fetch("../public/sitemap.xml");
 };

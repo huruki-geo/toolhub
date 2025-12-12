5fa1b56de33ebd7f91498204af3847773de78213
huruki-geo <lot.ofkzmnpc@gmail.com>
2025-12-13 07:58:26 +0900

    Update SEOHead.tsx

--- components/SEOHead.tsx
+++ components/SEOHead.tsx
@@ -1,12 +1,4 @@
-import React, { useEffect } from 'react';
-import { ViewState, Language } from '../types';
-import { TOOLS } from '../constants';
 
-interface Props {
-  view: ViewState;
-  lang: Language;
-}
-
-export const SEOHead: React.FC<Props> = ({ view, lang }) => {
+export const SEOHead {
   return null; // This component does not render UI
 };

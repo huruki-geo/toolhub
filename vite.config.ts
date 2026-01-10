17b7072b7ab8a86858d451ec6ca8572aea226d99
huruki-geo <lot.ofkzmnpc@gmail.com>
2026-01-10 10:44:57 +0900

    bd

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',     // ← これを必ず明示する
    build: {
     outDir: 'dist',
+     rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'legal-pages.html'),
+      },
+    }
   }
 });
